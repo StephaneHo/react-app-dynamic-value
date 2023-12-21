4395cf0d01104681528212dd2cd7e1588dd0f76c
Stéphane Hordoir <stephane.hordoir@gmail.com>
2023-12-21 16:15:04 +0100

    first commit

---
+++ src/solution/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+const equipments = [
+  "nacelle articulee",
+  "nacelle sur vehicule leger",
+  "gant bionique",
+];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Equipments() {
+  const equipment = equipments[genRandomInt(2)];
+  return <p>{equipment}</p>;
+}
+
+function App() {
+  return (
+    <>
+      <p>MAXOL</p>
+      <Equipments />
+    </>
+  );
+}
+
+export default App;
